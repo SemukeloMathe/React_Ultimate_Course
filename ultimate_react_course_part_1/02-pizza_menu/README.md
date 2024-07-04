02af71932efc10f210ce48940a126fdd0d100630
Semukelo Mathe <semmathe@gmail.com>
2024-07-04 10:59:19 +0200

    creating and reusing components

--- ultimate_react_course_part_1/02-pizza_menu/README.md
+++ ultimate_react_course_part_1/02-pizza_menu/README.md
@@ -1,10 +1,39 @@
-# Pizza Menu Reac
+# Pizza Menu App
 
 This project touches on the the following React Concepts:
 
-- Components
-- Props
-- JSX
-- Re-using components
-- Rendering Lists
-- Conditional Rendering
+## 1. Components
+
+- React applications are entirely made out of components.
+- They are the building blocks of User Interfaces in React.
+- It is a piece of UI that has it's own data, logic, and appearance.
+- We build complex UIs by building multiple component and combining them.
+- Components can be reused, nested inside each other, and pass data between them.
+- A component tree shows the hierachy that exists to show the components used.
+- When creating new components in react, we are obliged to use the following criteria:
+- 1. The component must be a function that starts with an uppercase letter.
+- 2. The function needs to return some mark up, usually in jsx but you can also return null.
+- 3. Each component needs to return one element.
+- 4. Never function component declarations, but always declare all your components in the top level.
+- 5. Use _fragments_ to render multiple components on the same element.
+
+## 2. Props
+
+## 3. JSX
+
+- A _declarative syntax_ to describe what components look like and how they work.
+- Components must return a block of JSX.
+- It is also an extension of JavaScript that allows us to _embed_ _JavaScript_, _CSS_, and _React components_ into _HTML_.
+- Each JSX element is converted to a _React.createElement_ function call.
+- We could use React without _JSX_.
+- JSX combines JavaScript, CSS, and HTML into one single block.
+
+## 4. Re-using components
+
+## 5. Rendering Lists
+
+## 6. Conditional Rendering
+
+## Key Notes
+
+React is an _abstraction_ away from the DOM, _We never touch the DOM_
--- ultimate_react_course_part_1/02-pizza_menu/src/index.js
+++ ultimate_react_course_part_1/02-pizza_menu/src/index.js
@@ -1,11 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import pizzaData from "./data.js";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+const App = () => {
+  return (
+    <section>
+      <Header />
+      <Menu />
+      <Footer />
+    </section>
+  );
+};
+
+
+function Header() {
+  return (
+    <h1>
+      Fast React Pizza Co.
+    </h1>
+  );
+};
+
+function Menu() {
+  return (
+    <section>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </section>
+  )
+};
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+  console.log(isOpen);
+  // else alert("Sorry, we're closed!");
+
+  return (
+    <footer>
+      {new Date().toLocaleTimeString()} We're closed!
+    </footer>
+  )
+};
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinach" />
+      <h2>Spinach Pizza</h2>
+      <p>Tomato, mozarello, spinach, and ricotta cheese</p>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>
+)
