b55c507dbf1f95df9a871129cbe44947b43551ac
Semukelo Mathe <semmathe@gmail.com>
2024-07-08 11:14:33 +0200

    handling events in react

--- ultimate_react_course_part_1/03-steps/README.md
+++ ultimate_react_course_part_1/03-steps/README.md
@@ -1,3 +1,14 @@
 # Steps App
 
 This project is a simple introduction on how we use _State_ in react.
+
+## Handling events in React
+
+- We directly listen on the element the event will happen on.
+- It's really important to specify a function.
+- We want to define a callback function that will be executed when the awaited event occurs, for example a mouse click event.
+
+## What is State?
+
+- Data that a component _can hold over time_.
+- It is basically a _Component's memory_.
--- ultimate_react_course_part_1/03-steps/src/index.js
+++ ultimate_react_course_part_1/03-steps/src/index.js
@@ -1,9 +1,35 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
-  return <h1 className="steps">Steps App</h1>
+  const [count, setCount] = useState(1);
+  // const count = 3;
+  const nextMessage = () => {
+    setCount(c => c + 1);
+  }
+  const prevMessage = () => {
+    setCount(c => c - 1);
+  }
+
+  return <div className="steps">
+    <div className="numbers">
+      <div className={`${count >= 1 ? "active" : ""}`}>1</div>
+      <div className={`${count >= 2 ? "active" : ""}`}>2</div>
+      <div className={`${count >= 3 ? "active" : ""}`}>3</div>
+    </div>
+    <p className="message">Step {count}: {messages[count - 1]}</p>
+    <div className="buttons">
+      <button style={{ backgroundColor: "#7950f2", color: "#FFF" }} onClick={prevMessage}>Prev</button>
+      <button style={{ backgroundColor: "#7950f2", color: "#FFF" }} onClick={nextMessage}>Next</button>
+    </div>
+  </div>
 };
 
 const root = createRoot(document.getElementById("root"));
