be4956da96b7655231510dffaf418bb204648bf7
Semukelo Mathe <semmathe@gmail.com>
2024-07-11 12:11:23 +0200

    date counter app

---
+++ ultimate_react_course_part_1/challenge_3-date_counter_advanced/README.md
@@ -0,0 +1,7 @@
+# Challenge 4 - Date Counter Advanced
+
+This challenge exposes your understanding of implememnting controlled elements in React.
+It improves the previous challenge and implements a form and a slider to set the state of the counter.
+
+In summary you learn how to control the same piece of state using multiple options, and then displaying that captured piece of state to the user.
+You also learn how to modify the Date using the Date methods.
--- ultimate_react_course_part_1/challenge_3-date_counter_advanced/src/index.js
+++ ultimate_react_course_part_1/challenge_3-date_counter_advanced/src/index.js
@@ -1,10 +1,71 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
 function App() {
-  return <h1>Hello World</h1>
-};
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  // const resetStats = () => {
+  //   setCount(0);
+  //   setStep(0);
+  // }
+
+  return (
+    <section className="app">
+      <article className="slider">
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <p>{step}</p>
+      </article>
+
+      <article className="buttons">
+        <button onClick={() => setCount(count - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button
+          onClick={() => {
+            setCount(count + step);
+          }}
+        >
+          +
+        </button>
+      </article>
+
+      <br />
+      <p className="date">
+        {count === 0 && `Today is ${date.toDateString()}`}
+        {count === 1 && `Tomorrow is ${date.toDateString()}`}
+        {count === -1 && `Yesterday was ${date.toDateString()}`}
+        {count < -1 && `${count} days ago was ${date.toDateString()}`}
+        {count > 1 && `${count} days from today is ${date.toDateString()}`}
+      </p>
+
+      <br />
+      {(step !== 1 || count !== 0) && (
+        <button
+          className="reset"
+          onClick={() => {
+            setCount(0);
+            setStep(1);
+          }}
+        >
+          Reset
+        </button>
+      )}
+    </section>
+  );
+}
 
 const root = createRoot(document.getElementById("root"));
 root.render(
