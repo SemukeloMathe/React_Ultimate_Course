70046c79ffc7a2f73008e2e6f2f2fdc53cfc2b00
Semukelo Mathe <semmathe@gmail.com>
2024-08-05 11:33:55 +0200

    building a reusable rating component

--- ultimate_react_course_part_2/07-use_popcorn/src/index.js
+++ ultimate_react_course_part_2/07-use_popcorn/src/index.js
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
-import "./index.css";
+// import App from "./App.js";
+// import "./index.css";
+import StarRating from "./StarRating";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
   </StrictMode>
 )
